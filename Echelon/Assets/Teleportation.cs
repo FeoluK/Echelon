3e3ecbd643c7104c8b551368cf3d03bcff414e0f
Feolu Kolawole <olufeolu@gmail.com>
2025-03-11 20:45:43 -0700

    minor changes

--- Echelon/Assets/Teleportation.cs
+++ Echelon/Assets/Teleportation.cs
@@ -7,29 +7,25 @@ public class ChangeScene : MonoBehaviour
 
     private void Start()
     {
-        DebugLogger.Log("ChangeScene Script Loaded - Waiting for Trigger...");
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other == null || other.gameObject == null)
         {
-            DebugLogger.Log("Trigger detected but GameObject is null!");
             return;
         }
 
         GameObject obj = other.gameObject;
-        DebugLogger.Log("Trigger Detected with: " + obj.name);
 
         // Check if the player entered the portal
         if (obj.CompareTag("Player"))
         {
-            DebugLogger.Log("Player entered the portal! Loading scene: " + destination);
             SceneManager.LoadScene(destination);
         }
         else
         {
-            DebugLogger.Log("⚠️ Trigger occurred, but NOT the player. Object: " + obj.name);
         }
     }
 }
